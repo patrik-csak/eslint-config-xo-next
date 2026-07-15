b0b392c05dad9c8d4b7db247cc0a889c2c11f7e0
Patrik Csak <p@trikcsak.com>
2026-07-15 11:08:40 -0700

    update xo and remove eslint-plugin-package-json

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,8 @@
 import {defineConfig, globalIgnores} from 'eslint/config';
 import xo from 'eslint-config-xo';
-import packageJson from 'eslint-plugin-package-json/experimental';
 
 export default defineConfig([
 	globalIgnores(['index.d.ts', 'index.js', 'package-lock.json']),
 
 	...xo(),
-
-	{
-		files: ['package.json'],
-		extends: [packageJson.configs.recommended, packageJson.configs.stylistic],
-	},
 ]);
